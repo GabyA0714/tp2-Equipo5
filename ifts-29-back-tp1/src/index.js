6191c9dd5d710e426f20c93187feac5ebfa207b4
GabyA0714 <143632333+GabyA0714@users.noreply.github.com>
2025-09-18 07:25:44 -0300

    Corrijo repo embebido y agrego proyecto ifts-29-back-tp1 como parte normal

---
+++ ifts-29-back-tp1/src/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const empleadoRoutes = require('./routes/empleadoRoutes')
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+// Middlewares
+app.use(express.json())
+
+// Rutas
+app.use('/api/empleados', empleadoRoutes)
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`)
+
+    const pacienteRoutes = require('./routes/pacienteRoutes');
+app.use('/pacientes', pacienteRoutes);
+
+})
