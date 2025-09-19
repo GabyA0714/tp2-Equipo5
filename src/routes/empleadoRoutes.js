eb32b217cdd033e03df8a247ed0e7f7f984a01cf
belencantarini <belencantarini@gmail.com>
2025-09-19 03:38:48 -0300

    TareasFix

--- src/routes/empleadoRoutes.js
+++ src/routes/empleadoRoutes.js
@@ -1,13 +1,14 @@
 const express = require('express')
-const { listarEmpleados, crearEmpleado, actualizarEmpleado, eliminarEmpleado, obtenerEmpleado } = require('../controllers/empleadoControllers')
+const empleadoControllers = require('../controllers/empleadoControllers')
 const { validarEmpleado } = require('../middlewares/validarEmpleado')
 
 const router = express.Router()
 
-router.get('/', listarEmpleados)
-router.post('/', validarEmpleado, crearEmpleado)
-router.put('/:id', validarEmpleado, actualizarEmpleado)
-router.delete('/:id', eliminarEmpleado)
-router.get('/:id', obtenerEmpleado)
+router.get('/', empleadoControllers.listarEmpleados)
+router.get('/nuevo', empleadoControllers.formularioNuevoEmpleado)
+router.post('/nuevo', validarEmpleado('nuevo'), empleadoControllers.crearEmpleado)
+router.get('/editar/:id', empleadoControllers.formularioEditarEmpleado)
+router.post('/editar/:id', validarEmpleado('editar'), empleadoControllers.actualizarEmpleado)
+router.delete('/eliminar/:id', empleadoControllers.eliminarEmpleado)
 
 module.exports = router
