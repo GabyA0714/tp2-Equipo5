734278e0ec30e434372deef0a476aa1fb524ff1b
GabyA0714 <143632333+GabyA0714@users.noreply.github.com>
2025-09-19 15:45:49 -0300

    Agregado CRUD y vistas Pug para pacientes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const path = require('path')
 const methodOverride = require('method-override')
+
 // API
 const apiEmpleadoRoutes = require('./routes/apiEmpleadoRoutes')
-const pacienteRoutes = require('./routes/pacienteRoutes')
+const apiPacienteRoutes = require('./routes/apiPacienteRoutes') 
 const apiTareaRoutes = require('./routes/apiTareaRoutes')
+
 // Vistas
 const empleadoRoutes = require('./routes/empleadoRoutes')
 const tareaRoutes = require('./routes/tareaRoutes')
+// Si después crean vistas para pacientes:
+// const pacienteRoutes = require('./routes/pacienteRoutes')
 
 const PORT = process.env.PORT || 5000
 const app = express()
@@ -18,17 +22,18 @@ app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 // Pug
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "views"))
 app.set('view engine', 'pug')
 
 // Rutas API
 app.use('/api/empleados', apiEmpleadoRoutes)
-app.use('/api/pacientes', pacienteRoutes)
+app.use('/api/pacientes', apiPacienteRoutes) 
 app.use('/api/tareas', apiTareaRoutes)
 
 // Rutas Vistas
 app.use('/empleados', empleadoRoutes)
 app.use('/tareas', tareaRoutes)
+// app.use('/pacientes', pacienteRoutes) 
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en puerto ${PORT}`)
