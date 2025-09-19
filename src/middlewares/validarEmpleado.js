985cec24068cab8d00edde2500dec6951585d186
Facundo Azcue <fazcue@gmail.com>
2025-09-19 14:13:02 -0300

    Mejora de lógica + Añadido título + Mensajes de error

--- src/middlewares/validarEmpleado.js
+++ src/middlewares/validarEmpleado.js
@@ -2,46 +2,38 @@ const { leerData } = require('../lib/fs')
 
 function validarEmpleado(vista) {
 	const url = `empleados/${vista}`
+	const titulo = vista.charAt(0).toUpperCase() + vista.slice(1) + " empleado"
 
 	return async (req, res, next) => {
+		let error = ""
+		
 		// Leer data
 		const roles = await leerData('roles')
 		const areas = await leerData('areas')
 		const empleados = await leerData('empleados')
 
-		const { nombre, apellido, dni, email, rol, area } = req.body
-		const { id } = req.params
+		const { rol, area } = req.body
 
-		// Generar empleado con/sin id
-		let empleado = {}
-		if (id) {
-			empleado = { id, nombre, apellido, dni, email, rol, area }
-		} else {
-			empleado = { nombre, apellido, dni, email, rol, area }
-		}
+		// Generar empleado con id (de existir)
+		const empleado = { ...req.body, id: req.params.id }
 
 		// Validar DNI
 		if (empleados.some((e) => e.dni === empleado.dni && e.id !== empleado.id)) {
-			return res.render(url, {
-				error: 'DNI registrado previamente',
-				empleado,
-			})
+			error = 'DNI registrado previamente'
 		}
 
 		// Validar rol
 		if (!roles.includes(rol)) {
-			return res.render(url, {
-				error: `Rol inválido. Debe ser uno de: ${roles.join(', ')}`,
-				empleado,
-			})
+			error = `Rol inválido. Debe ser uno de: ${roles.join(', ')}`
 		}
 
 		// Validar área
 		if (!areas.includes(area)) {
-			return res.render(url, {
-				error: `Área inválida. Debe ser una de: ${areas.join(', ')}`,
-				empleado,
-			})
+			error = `Área inválida. Debe ser una de: ${areas.join(', ')}`
+		}
+
+		if (error) {
+			return res.render(url, { titulo, error, empleado })
 		}
 
 		next()
